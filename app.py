5f1204a49695d05fad0d92b9663968dcea12ecc5
John Ewan <j-ewan@northwestern.edu>
2018-05-05 00:12:15 -0500

    Mars - almost done!

--- app.py
+++ app.py
@@ -1,36 +1,81 @@
 from flask import Flask, render_template, jsonify, redirect
-#from flask_pymongo import PyMongo
-import pymongo
 import scrape_mars
+import pymongo
 
 app = Flask(__name__)
 
-##mongo = PyMongo(app)
+# The default port used by MongoDB is 27017
+# https://docs.mongodb.com/manual/reference/default-mongodb-port/
 conn = 'mongodb://localhost:27017'
 client = pymongo.MongoClient(conn)
-db = client.mars_hw_jre
-collection = db.collection
 
-#mars_news_dict = {'title': 'NASA...', 'mn_para': 'NASA’s next ...'}
+# Declare the database
+db = client.mars_hw_jre
+
+# Declare the collection
+collection = db.mars_news
+
+mars_dict = {"mn_title": "NASA...",
+             "mn_para": "NASA’s next ...",
+             "mfi": "https://static3.srcdn.com/wp-content/uploads/Tim-Burtons-Mars-Attacks.jpg?q=50&w=1000&h=500&fit=crop",
+             "mw": "current martian weather",
+             "mf_ed": "wide",
+             "mf_pd": "high",
+             "mf_m": "heavy",
+             "mf_moons": "yes",
+             "mf_od": "around",
+             "mf_op": "martian year",
+             "mf_st": "not hot",
+             "mf_fr": "ages ago",
+             "mf_rb":  "king tut",
+             "mh1_title": "Mars Attacks",
+             "mh1_link": "https://theawesomer.com/photos/2012/11/110912_mars_attacks_ornament_1.jpg",
+             "mh2_title": "and Attacks",
+             "mh2_link": "http://www.flore-maquin.com/wp-content/uploads/Mars_Attack_FLOREMAQUIN.jpg",
+             "mh3_title": "and Attacks",
+             "mh3_link": "https://mfiles.alphacoders.com/611/611833.jpg",
+             "mh4_title": "and it ends",
+             "mh4_link": "https://i.pinimg.com/564x/1e/94/a5/1e94a52c15a5a266e95792dbdc02d529.jpg"}
 
 @app.route("/")
 def index():
-    listings = db.collection.find_one()
-    #return render_template("index.html",  dict = mars_news_dict, listings = listings)
-    return render_template("index.html",  listings = listings)
+    return render_template("index.html",  dict=mars_dict)
     
 @app.route("/scrape")
 def scrape():
-    #listings = mongo.db.listings
-    listings = db.collection
-    listings_data = scrape_mars.mars_news()
-    listings.update(
-        {},
-        listings_data,
-        upsert=True
-    )
+    # mars news
+    mars_news_junk = scrape_mars.mars_news()
+    mars_dict["mn_title"] = mars_news_junk[0]
+    mars_dict["mn_para"] = mars_news_junk[1]
+    # featured image
+    mars_featured_image_link = scrape_mars.mars_feat_image()
+    mars_dict["mfi"] = mars_featured_image_link
+    # mars weather
+    mars_latest_weather = scrape_mars.mars_weather()
+    mars_dict["mw"] = mars_latest_weather
+    # mars hemispheres
+    # hemi 1
+    mars_hemi1_list = scrape_mars.mars_hemi1()
+    mars_dict["mh1_title"] = mars_hemi1_list[0]
+    mars_dict["mh1_link"] = mars_hemi1_list[1]
+    # hemi 2
+    mars_hemi2_list = scrape_mars.mars_hemi2()
+    mars_dict["mh2_title"] = mars_hemi2_list[0]
+    mars_dict["mh2_link"] = mars_hemi2_list[1]
+    # hemi 3
+    mars_hemi3_list = scrape_mars.mars_hemi3()
+    mars_dict["mh3_title"] = mars_hemi3_list[0]
+    mars_dict["mh3_link"] = mars_hemi3_list[1]
+    # hemi4
+    mars_hemi4_list = scrape_mars.mars_hemi4()
+    mars_dict["mh4_title"] = mars_hemi4_list[0]
+    mars_dict["mh4_link"] = mars_hemi4_list[1]
+
+    # insert the updated dictionary into Mongo
+    # Insert the document into the database
+    collection.insert_one(mars_dict)
+    
     return redirect("http://localhost:5000/", code=302)
-
-
+ 
 if __name__ == "__main__":
     app.run(debug=True)
--- scrape_mars.py
+++ scrape_mars.py
@@ -6,12 +6,13 @@ from selenium import webdriver
 import requests
 
 
-
+# Mars news
 def mars_news():
+    mars_junk = []
     # Scrape NASA Mars News
     # URL of page to be scraped
     url = 'https://mars.nasa.gov/news/'
-    listings = {}
+    mars_stuff = {}
     # scrape & fire up the BS!
     browser = webdriver.Chrome('chromedriver.exe')
     # get the page
@@ -25,10 +26,175 @@ def mars_news():
     headline = soup.find('div', class_="content_title")
     first_para = soup.find('div', class_="article_teaser_body")
     # grab the title & print it!
-    listings["title"] = soup.find('div', class_="content_title").text
-    listings["mn_para"] = soup.find('div', class_="article_teaser_body").text
+    headline_txt = soup.find('div', class_="content_title").text
+    first_para_txt = soup.find('div', class_="article_teaser_body").text
+    mars_junk.append(headline_txt)
+    mars_junk.append(first_para_txt)
+        
+    return mars_junk
+
+# JPL Featured Image
+def mars_feat_image():
+    # Scrape JPL Mars Space Images - Featured Image
+    # URL of page to be scraped
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    feat_image_link = ""
+    browser = webdriver.Chrome('chromedriver.exe')
+    #listings = {}
+    # get the page
+    browser.get(url)
+    #Wait a few seconds
+    time.sleep(3)
+
+    html = browser.page_source
+    soup = BeautifulSoup(html, "html.parser")
+    # get the first result
+    feature_img = soup.find('a', class_="button fancybox")
+    #turn it into a string and split it on the item that hold the link value
+    splitsville = str(feature_img).split("data-fancybox-href=")
+    # grab the first part of the splitsville[1] (i.e. the 2nd part of the link)
+    url_part2 = splitsville[1].split(" ")
+    # trim off the quotes
+    part2 = url_part2[0].replace('"','')
+    feat_image_link = "https://www.jpl.nasa.gov" + part2
+
+    return feat_image_link
+
+# Weather
+def mars_weather():
+    # Scrape twitter mars weather
+    # URL of page to be scraped
+    url = "http://twitter.com/marswxreport?lang=en"
+    weather_junk = ""
+    browser = webdriver.Chrome('chromedriver.exe')
+    # get the page
+    browser.get(url)
+    #Wait a few seconds
+    time.sleep(3)
+    html = browser.page_source
+    soup = BeautifulSoup(html, "html.parser")
+    # get the first result
+    weathertweet = soup.find('div', class_="js-tweet-test-container")
+    #return the tweet
+    weather_junk = soup.find('p', class_="TweetTextSize TweetTextSize--normal js-tweet-text tweet-text").text
     
-    return listings
+    return weather_junk
+
+# Mars facts
+def mars_facts():
+    mars_facts_list = []
+    # Mars facts
+    # url of page to be scraped
+    url = "http://space-facts.com/mars/"
+
+
+    return mars_facts_list
+
+# Mars Hemispheres    
+def mars_hemi1():
+    mars_hemi1 = []
+    base_url = "https://astrogeology.usgs.gov"
+    url_valles = "https://astrogeology.usgs.gov/search/map/Mars/Viking/valles_marineris_enhanced"
+    # Valles Marineris Hemisphere
+    browser = webdriver.Chrome('chromedriver.exe')
+    # get the page
+    browser.get(url_valles)
+    #Wait a few seconds
+    time.sleep(7)
+    html = browser.page_source
+    soup = BeautifulSoup(html, "html.parser")
+    # Valles Marineris Hemisphere 2 - get the big image
+    valles_speclink = soup.find('img', class_="wide-image")
+    #turn it into a string and split it on the item that hold the link value
+    splitsville = str(valles_speclink).split("src=")
+    url_part2 = splitsville[1].split("/>")
+    # trim off the quotes
+    part2 = url_part2[0].replace('"','')
+    valles_full_img = base_url + part2
+    valles_title = "Valles Marineris Hemisphere"
+    mars_hemi1.append(valles_title)
+    mars_hemi1.append(valles_full_img)
+
+    return mars_hemi1
+
+def mars_hemi2():
+    mars_hemi2 = []
+    base_url = "https://astrogeology.usgs.gov"
+    url_cerberus = "https://astrogeology.usgs.gov/search/map/Mars/Viking/cerberus_enhanced"
+    # Cerberus Marineris Hemisphere
+    browser = webdriver.Chrome('chromedriver.exe')
+    # get the page
+    browser.get(url_cerberus)
+    #Wait a few seconds
+    time.sleep(7)
+    html = browser.page_source
+    soup = BeautifulSoup(html, "html.parser")
+    # Cerberus Hemisphere 2 - get the big image
+    cerberus_speclink = soup.find('img', class_="wide-image")
+    #turn it into a string and split it on the item that hold the link value
+    splitsville = str(cerberus_speclink).split("src=")
+    url_part2 = splitsville[1].split("/>")
+    # trim off the quotes
+    part2 = url_part2[0].replace('"','')
+    cerberus_full_img = base_url + part2
+    cerberus_title = "Cerberus Hemisphere"
+    mars_hemi2.append(cerberus_title)
+    mars_hemi2.append(cerberus_full_img)
+
+    return mars_hemi2
+
+def mars_hemi3():
+    mars_hemi3 = []
+    base_url = "https://astrogeology.usgs.gov"
+    url_schiaparelli = "https://astrogeology.usgs.gov/search/map/Mars/Viking/schiaparelli_enhanced"
+    # Schiaparelli Hemisphere
+    browser = webdriver.Chrome('chromedriver.exe')
+    # get the page
+    browser.get(url_schiaparelli)
+    #Wait a few seconds
+    time.sleep(7)
+    html = browser.page_source
+    soup = BeautifulSoup(html, "html.parser")
+    # Schiaparelli Hemisphere 2 - get the big image
+    schia_speclink = soup.find('img', class_="wide-image")
+    #turn it into a string and split it on the item that hold the link value
+    splitsville = str(schia_speclink).split("src=")
+    url_part2 = splitsville[1].split("/>")
+    # trim off the quotes
+    part2 = url_part2[0].replace('"','')
+    schia_full_img = base_url + part2
+    schia_title = "Schiaparelli Hemisphere"
+    mars_hemi3.append(schia_title)
+    mars_hemi3.append(schia_full_img)
+
+    return mars_hemi3
+    
+def mars_hemi4():
+    mars_hemi4 = []
+    base_url = "https://astrogeology.usgs.gov"
+    url_syrtis = "https://astrogeology.usgs.gov/search/map/Mars/Viking/syrtis_major_enhanced"
+    
+    # Syrtis Major Hemisphere
+    browser = webdriver.Chrome('chromedriver.exe')
+    # get the page
+    browser.get(url_syrtis)
+    #Wait a few seconds
+    time.sleep(7)
+    html = browser.page_source
+    soup = BeautifulSoup(html, "html.parser")
+    # Syrtis Major Hemisphere 2 - get the big image
+    syrtis_speclink = soup.find('img', class_="wide-image")
+    #turn it into a string and split it on the item that hold the link value
+    splitsville = str(syrtis_speclink).split("src=")
+    url_part2 = splitsville[1].split("/>")
+    # trim off the quotes
+    part2 = url_part2[0].replace('"','')
+    syrtis_full_img = base_url + part2
+    syrtis_title = "Syrtis Major Hemisphere"
+    mars_hemi4.append(syrtis_title)
+    mars_hemi4.append(syrtis_full_img)
+
+    return mars_hemi4
 
 
     
